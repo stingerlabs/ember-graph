e73e4d5a3f1127afa053d99cb356801e5a367b4c
Gordon Kristan <gordon.kristan@gmail.com>
2014-05-19 09:00:45 -0400

    Improved UglifyJS options.

--- tasks/options/uglify.js
+++ tasks/options/uglify.js
@@ -1,7 +1,33 @@
 module.exports = {
 	options: {
-		mangle: false,
-		compress: true,
+		mangle: true,
+		compress: {
+			global_defs: {
+				DEBUG: false,
+				RELEASE: true
+			},
+			sequences: true,
+			properties: true,
+			drop_debugger: true,
+			unsafe: false,
+			conditionals: true,
+			comparisons: true,
+			evaluate: true,
+			booleans: true,
+			dead_code: true,
+			loops: true,
+			unused: true,
+			hoist_funs: false,
+			hoist_vars: false,
+			if_return: true,
+			join_vars: true,
+			cascade: true,
+			warnings: true,
+			negate_iife: false,
+			pure_getters: false,
+			pure_funcs: null,
+			drop_console: true
+		},
 		screw_ie8: true
 	},
 
