8ef48e4910ab8336b8d02e94a8e17e879469ea97
Gordon Kristan <gordon.kristan@gmail.com>
2014-06-10 11:40:42 -0400

    Add some utility functions.

--- src/util/util.js
+++ src/util/util.js
@@ -1,11 +1,23 @@
+EG.required = function(methodName) {
+	return function() {
+		throw new Error('You failed to implement the abstract `' + methodName + '` method.');
+	};
+};
+
+EG.generateUUID = function() {
+	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+		var r = Math.random()*16|0; // jshint ignore:line
+		var v = (c == 'x' ? r : (r&0x3|0x8)); // jshint ignore:line
+		return v.toString(16);
+	});
+};
+
 EG.util = {
-	generateGUID: function() {
-		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-			var r = Math.random()*16|0; // jshint ignore:line
-			var v = (c == 'x' ? r : (r&0x3|0x8)); // jshint ignore:line
-			return v.toString(16);
-		});
-	},
+
+	/**
+	 * @deprecated
+	 */
+	generateGUID: EG.generateUUID,
 
 	/**
 	 * @deprecated
