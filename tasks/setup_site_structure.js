e8b26066a8072c0d39612af46755b43dbd2e7248
Gordon Kristan <gordon.kristan@gmail.com>
2014-06-13 18:19:51 -0400

    Add redirect base page.

--- tasks/setup_site_structure.js
+++ tasks/setup_site_structure.js
@@ -10,6 +10,7 @@ module.exports = function(grunt) {
 		sh.run('cp -r site_assets/fonts site/');
 		sh.run('cp -r site_assets/javascripts site/');
 		sh.run('cp -r site_assets/CNAME site/');
+		sh.run('cp -r site_assets/index.html site/');
 
 		sh.run('mkdir -p site/stylesheets');
 		sh.run('cp -r site_assets/stylesheets/style.css site/stylesheets/');
