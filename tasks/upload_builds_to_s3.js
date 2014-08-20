11164abe35ac11223c41f5b8497f00922b31bc66
Gordon Kristan <gordon.kristan@gmail.com>
2014-08-20 13:24:01 -0400

    Add zipped archives for easy Bower access.

--- tasks/upload_builds_to_s3.js
+++ tasks/upload_builds_to_s3.js
@@ -3,6 +3,7 @@
 var fs = require('fs');
 var sh = require('execSync');
 var AWS = require('aws-sdk');
+var AdmZip = require('adm-zip');
 
 module.exports = function(grunt) {
 	grunt.registerTask('upload_builds_to_s3', function() {
@@ -12,6 +13,12 @@ module.exports = function(grunt) {
 		var productionBuild = fs.readFileSync('./dist/ember-graph.prod.js', { encoding: 'utf8' });
 		var minifiedBuild = fs.readFileSync('./dist/ember-graph.min.js', { encoding: 'utf8' });
 
+		var zip = new AdmZip();
+		zip.addFile('ember-graph.js', new Buffer(debugBuild));
+		zip.addFile('ember-graph.prod.js', new Buffer(productionBuild));
+		zip.addFile('ember-graph.min.js', new Buffer(minifiedBuild));
+		var archive = zip.toBuffer();
+
 		var count = 0;
 		var counter = function(success, fileName) {
 			count = count + 1;
@@ -20,18 +27,21 @@ module.exports = function(grunt) {
 				console.log('Error uploading file to S3: ' + fileName);
 			}
 
-			if (count >= 6) {
+			if (count >= 8) {
 				done();
 			}
 		};
 
 		var s3 = new AWS.S3();
 		uploadFile(s3, 'latest/ember-graph.js', debugBuild, counter);
-		uploadFile(s3, hash + '/ember-graph.js', debugBuild, counter);
 		uploadFile(s3, 'latest/ember-graph.prod.js', productionBuild, counter);
-		uploadFile(s3, hash + '/ember-graph.prod.js', productionBuild, counter);
 		uploadFile(s3, 'latest/ember-graph.min.js', minifiedBuild, counter);
+		uploadFile(s3, 'latest/ember-graph.zip', archive, counter);
+
 		uploadFile(s3, hash + '/ember-graph.min.js', minifiedBuild, counter);
+		uploadFile(s3, hash + '/ember-graph.js', debugBuild, counter);
+		uploadFile(s3, hash + '/ember-graph.prod.js', productionBuild, counter);
+		uploadFile(s3, hash + '/ember-graph.zip', archive, counter);
 	});
 };
 
